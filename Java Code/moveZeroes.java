851763fe5f28f82c771b6d3c7f1d2727a63cd194
Kunal Avinash Kanse <kunalkanse2006@gmail.com>
2026-02-13 23:26:12 +0530

    Create moveZeroes.java
    This program takes an array nums as input and move all 0's present in it at the end without changing the relative order of non-zero elements.

---
+++ Java Code/moveZeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int writeIndex = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != 0){
+                nums[writeIndex] = nums[i];
+                writeIndex++;
+            }
+        }
+        for(int i = writeIndex; i < nums.length; i++){
+            nums[i] = 0;
+        }
+    }
+}
